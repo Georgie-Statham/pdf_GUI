eb48729d65f16c43fba3330bb4fa10f451bed3ae
STATHAM GEORGIE <georgie.statham@gmail.com>
2019-07-18 17:22:06 +0100

    join.py first draft complete

--- join.py
+++ join.py
@@ -8,6 +8,7 @@ class JoinPanel(wx.Panel):
 
     def __init__(self, parent):
         super().__init__(parent)
+        self.input_paths = []
         main_sizer = wx.BoxSizer(wx.VERTICAL)
 
         open_button = wx.Button(self, 5, "Select pdfs")
@@ -15,16 +16,14 @@ class JoinPanel(wx.Panel):
         main_sizer.Add(open_button, 0, wx.ALL, 5)
 
         header = wx.StaticText(self, 0, "Selected:")
-        main_sizer.Add(header, 0)
+        main_sizer.Add(header, 0, wx.LEFT, 5)
 
-        self.input_pdfs = wx.TextCtrl(self, 0)
-        self.input_pdfs.Disable()
-        main_sizer.Add(self.input_pdfs, 5, wx.ALIGN_CENTER|wx.ALL, 5)
+        self.input_files = wx.TextCtrl(self, 0, style=wx.TE_MULTILINE)
+        self.input_files.Disable()
+        main_sizer.Add(
+            self.input_files, 5, wx.ALIGN_CENTER|wx.ALL|wx.EXPAND, 5)
 
-        join_button = wx.Button(self, 0, "Join")
-        main_sizer.Add(join_button, 0, wx.ALIGN_RIGHT|wx.ALL, 5)
-
-        save_button = wx.Button(self, 0, "Save as...")
+        save_button = wx.Button(self, 0, "Join and save")
         save_button.Bind(wx.EVT_BUTTON, self.SaveButton)
         main_sizer.Add(save_button, 0,  wx.ALIGN_RIGHT|wx.ALL, 5)
 
@@ -45,10 +44,11 @@ class JoinPanel(wx.Panel):
         )
 
         if dlg.ShowModal() == wx.ID_OK:
-            input_paths = dlg.GetPaths() # pass to textctrl
+            for file in dlg.GetFilenames():
+                self.input_files.AppendText(file + '\n')
+            for path in dlg.GetPaths():
+                self.input_paths.append(path)
             dlg.Destroy()
-
-    # list
 
 
     def SaveButton(self, event):
@@ -62,28 +62,29 @@ class JoinPanel(wx.Panel):
         )
 
         if dlg.ShowModal() == wx.ID_OK:
-            path = dlg.GetPath()
+            output_path = dlg.GetPath()
 
-        # with open(path, 'wb') as output:
-        #         pdf_writer = PdfFileWriter()
+            pdf_writer = PdfFileWriter()
 
-        # for path in input_paths: # get from interactive list
-        #     pdf_reader = PdfFileReader(path)
-        #     for page in range(pdf_reader.getNumPages()):
-        #         pdf_writer.addPage(pdf_reader.getPage(page))
+            for path in self.input_paths:
+                pdf_reader = PdfFileReader(path)
+                for page in range(pdf_reader.getNumPages()):
+                    pdf_writer.addPage(pdf_reader.getPage(page))
 
-        # pdf_writer.write(output)
-        dlg.Destroy()
+            with open(output_path, 'wb') as output:
+                pdf_writer.write(output)
+
+            dlg.Destroy()
 
 class JoinFrame(wx.Frame):
     def __init__(self):
         super().__init__(
             None,
             title="Join pdfs",
-            size=(200, 250),
+            size=(400, 250),
         )
         panel = JoinPanel(self)
-        self.SetSizeHints(200, 250, 200, 250)
+        self.SetSizeHints(400, 250, 400, 250)
         self.Show()
 
 if __name__ == "__main__":
